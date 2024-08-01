70af0a1cc4b1a706c4ccbe151ee5c273e93eff9b
Pragya Saxena <pragyasaxena8279@gmail.com>
2024-08-01 09:22:07 +0530

    implemented lazy loading

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import List from '../components/List'
-import Categories from '../components/Categories'
-import Carousel from '../components/Carousel'
+import React, { lazy, Suspense } from 'react';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+
+const List = lazy(() => import('../components/List'));
 
 export default function Home() {
-
   return (
     <>
-      <Carousel/>
-      {/* <List id='443' showPlayButton={true} /> */}
-      {/* <List id='1003' showPlayButton={true} /> */}
-      <List id='285' showPlayButton={true} listStyles={{marginTop:'40px'}}/>
-      <Categories/>
-      <List id='410' showPlayButton={false} listStyles={{marginTop:'40px'}}/>
-      <List id='380' showPlayButton={false} listStyles={{marginTop:'30px'}}/>
-      <List id='378' showPlayButton={false} listStyles={{marginTop:'60px'}}/>
-      <List id='377' showPlayButton={false} listStyles={{marginTop:'30px'}}/>
+      <Carousel />
+      <Suspense fallback={<div style={{textAlign: 'center'}}>Please wait...</div>}>
+        <List id='285' showPlayButton={true} listStyles={{ marginTop: '40px' }} />
+      </Suspense>
+      <Categories />
+      <Suspense fallback={<div style={{textAlign: 'center'}}>Please wait...</div>}>
+        <List id='410' showPlayButton={false} listStyles={{ marginTop: '40px' }} />
+      </Suspense>
+      <Suspense fallback={<div style={{textAlign: 'center'}}>Please wait...</div>}>
+        <List id='380' showPlayButton={false} listStyles={{ marginTop: '30px' }} />
+      </Suspense>
+      <Suspense fallback={<div style={{textAlign: 'center'}}>Please wait...</div>}>
+        <List id='378' showPlayButton={false} listStyles={{ marginTop: '60px' }} />
+      </Suspense>
+      <Suspense fallback={<div style={{textAlign: 'center'}}>Please wait...</div>}>
+        <List id='377' showPlayButton={false} listStyles={{ marginTop: '30px' }} />
+      </Suspense>
     </>
-  )
+  );
 }
